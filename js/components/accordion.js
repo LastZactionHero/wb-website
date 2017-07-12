db16412430257091b30e702eb663a6ee877cd427
Zach Dicklin <zach@workbright.com>
2017-07-12 15:48:31 -0600

    Update accordion scroll position to accomodate new navbar

--- js/components/accordion.js
+++ js/components/accordion.js
@@ -1,7 +1,7 @@
 // Accordion scroll to the active card
 $(".accordion .card-header a").click( function(event) {
   setTimeout(function() {    
-    const scrollOffset = 64;
+    const scrollOffset = 160;
     const scrollTo = $(event.target).offset().top - scrollOffset;
     $('html,body').animate({scrollTop: scrollTo}, 250);
   }, 200);
