9c679f230a8e10f0d734f2c5228beb3df04e4a4b
Patrick Navarro <patricknavarro@gmail.com>
2017-08-02 14:51:41 -0600

    Remove GrowSumo from pardot.js

--- js/pardot.js
+++ js/pardot.js
@@ -8,33 +8,3 @@ for (var i = 0; i < formInputs.length; i++) {
 }
 
 import iFrameResize from "../node_modules/iframe-resizer/js/iframeResizer.contentWindow.js";
-
-// GrowSumo Start
-window.onload = function () {
-  parent.window.postMessage(true, '*');
-
-  form = document.getElementById("pardot-form");
-  
-  if (form) {
-    form.setAttribute("onsubmit", "return pushGrowSumo()");
-  }
-};
-
-function pushGrowSumo() {
-  var fName = document.getElementById("81162_111591pi_81162_111591").value;
-  var lName = document.getElementById("81162_111593pi_81162_111593").value;
-  var email = document.getElementById('81162_111597pi_81162_111597').value;
-  var apiPublicKey = 'pk_d83595336d74496294853b971de5df8f';
-
-  if (email.length && fName.length && lName.length) {
-      var formDetails = {
-  		firstName: fName,
-  		lastName: lName,
-  		email: email,
-  		publicKey: apiPublicKey,
-  	}
-      // send the main website the form details we can avoid CORS issues from POSTing to growsumo from this pardot view
-  	parent.window.postMessage(formDetails, '*');
-  }
-};
-// Growsumo End
